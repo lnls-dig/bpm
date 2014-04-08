f1b49cc4f8b8b5964e5346d0f30627b21a4eb950
Daniel Tavares <daniel.tavares@lnls.br>
2014-04-08 10:30:37 -0300

    ddc_sim.m: change window input of fourierseries function
    According to the new behavior of the fourierseries function, the window input shall be a vector instead of function handle.

--- ddc_sim.m
+++ ddc_sim.m
@@ -131,14 +131,15 @@ calc_pos_s = K*(a_mag_s-c_mag_s)./(a_mag_s+c_mag_s);
 
 %% Plotting
 % Compute FFTs
-[MAF_a_mag, ff_a_mag] = fourierseries(a_mag(50:end), Ffofb, @nuttallwin);
-[MAF_c_mag, ff_c_mag] = fourierseries(c_mag(50:end), Ffofb, @nuttallwin);
-[MAF_a_mag_s, ff_a_mag_s] = fourierseries(a_mag_s(50:end), Ffofb, @nuttallwin);
-[MAF_c_mag_s, ff_c_mag_s] = fourierseries(c_mag_s(50:end), Ffofb, @nuttallwin);
+window = nuttallwin(length(a_mag(50:end)));
+[MAF_a_mag, ff_a_mag] = fourierseries(a_mag(50:end), Ffofb, window);
+[MAF_c_mag, ff_c_mag] = fourierseries(c_mag(50:end), Ffofb, window);
+[MAF_a_mag_s, ff_a_mag_s] = fourierseries(a_mag_s(50:end), Ffofb, window);
+[MAF_c_mag_s, ff_c_mag_s] = fourierseries(c_mag_s(50:end), Ffofb, window);
 
 % Ignore the first samples to avoid filter transient
-[MAF_calc_pos, ff_calc_pos] = fourierseries(calc_pos(50:end), Ffofb, @nuttallwin);
-[MAF_calc_pos_s, ff_calc_pos_s] = fourierseries(calc_pos_s(50:end), Ffofb, @nuttallwin);
+[MAF_calc_pos, ff_calc_pos] = fourierseries(calc_pos(50:end), Ffofb, window);
+[MAF_calc_pos_s, ff_calc_pos_s] = fourierseries(calc_pos_s(50:end), Ffofb, window);
 
 % General coloring rules: unswitched signal : green, switched: red,
 % sausaged: blue. If overplotting, the second color may be yellow,
