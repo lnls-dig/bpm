17e1fb0c4354df13169179817d8c45539fdf53ea
Daniel Tavares <daniel.tavares@lnls.br>
2014-04-17 13:20:31 -0300

    pnmodel.m: rename vcomodel.m to pnmodel.m to be more generic

--- clock/pnmodel.m
+++ clock/pnmodel.m
@@ -1,4 +1,4 @@
-function [P, f] = vcomodel(modelname)
+function [P, f] = pnmodel(modelname)
 
 if strcmpi(modelname, 'cvhd950')
     f = [logspace(log10(10), log10(100), 100) logspace(log10(100), log10(1e3), 100) logspace(log10(1e3), log10(10e3), 100) logspace(log10(10e3), log10(1e6), 100) 2e6];
@@ -6,9 +6,9 @@ if strcmpi(modelname, 'cvhd950')
 elseif strcmpi(modelname, 'si571')
     f = [logspace(log10(100), log10(1e3), 100) logspace(log10(1e3), log10(10e3), 100) logspace(log10(10e3), log10(100e3), 100) logspace(log10(100e3), log10(1e6), 100) logspace(log10(1e6), log10(10e6), 100) 20e6];
     P = [-linspace(87, 114, 100) -linspace(114, 132, 100) -linspace(132, 142, 100) -linspace(142, 148, 100) -linspace(148,150, 100) -150];
-elseif strcmpi(modelname, 'test')
-    f = [logspace(log10(100), log10(1e3), 100) logspace(log10(1e3), log10(10e3), 100) logspace(log10(10e3), log10(100e3), 100) logspace(log10(100e3), log10(1e6), 100) logspace(log10(1e6), log10(10e6), 100) 10e6];
-    P = 20+[-linspace(87, 114, 100) -linspace(114, 132, 100) -linspace(132, 142, 100) -linspace(142, 148, 100) -linspace(148,150, 100) -150];
+elseif strcmpi(modelname, 'ad9510_clkdist')
+    f = [100e3 10e6];
+    P = [-150 -150];
 else
-    error('A valid VCO model name must be specified.');
+    error('A valid phase noise model name must be specified.');
 end
