81617ae03e36338eec3f913a7026b56b5cf0cc37
Vfinotti <finotao@gmail.com>
2015-06-25 19:17:27 -0300

    Added script that draws the button gain ilustration.

--- button_gain_delta_sigma_sketch.m
+++ button_gain_delta_sigma_sketch.m
@@ -3,12 +3,15 @@
 close all;
 clear all;
 
-t = 0:0.01:1;
+t = -0.5:0.0001:1;
 
-aw = 0.25*square(2*pi*3.5*t)+0.5;
+al = 0.25;
+aw = 0.125*square(2*pi*3.5/2*t)+al;
 
-cw = 0.25*square(2*pi*3.5*t)-0.5;
+cl = -0.25;
+cw = 0.125*square(2*pi*3.5/2*t+pi)+cl;
 
-
-plot(t,aw,t,cw)
-% axis([-1 1 -1 1])
+plot(t,aw,t,cw,[t(1) t(end)*1.2],[al al],'r--',[t(1) t(end)*1.2],[cl cl],'r--')
+axis equal
+axis([-1 1.2 -0.6 0.6])
+set(gca,'xtick',[],'ytick',[])
