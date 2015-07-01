aac5e51eb028033d3a8629b20e9c3775127087b6
Vfinotti <finotao@gmail.com>
2015-07-01 10:28:38 -0300

    Started implementing Daphine's VHDL algorithm.

---
+++ daphine.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity daphine is
+  port ( a,b,c,d : in real;
+         x,y :out real );
+
+end entity
+
+
+architecture daphine_arch of daphine is
+  begin
+    x <= ((a-c)/(a+c)+(d-b)/(d+b))*Kx;
+    y <= ((a-c)/(a+c)-(d-b)/(d+b))*Ky;
+
+end daphine_arch
+
+      
