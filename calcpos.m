a87ba915f3183a19a07aff8a48edce991ddc921a
Daniel Tavares <daniel.tavares@lnls.br>
2016-01-13 10:42:38 -0200

    calcpos.m: add new method for calculating beam position, partial delta over sigma

--- calcpos.m
+++ calcpos.m
@@ -1,4 +1,4 @@
-function [xy, q, s] = calcpos(abcd, Kx, Ky, Ks)
+function [xy, q, s] = calcpos(abcd, Kx, Ky, Ks, method)
 
 if nargin < 2
     Kx = 1;
@@ -9,18 +9,34 @@ end
 if nargin < 4
     Ks = 1;
 end
+if nargin < 5
+    method = 'delta/sigma';
+end
 
 a = abcd(:,1);
 b = abcd(:,2);
 c = abcd(:,3);
 d = abcd(:,4);
 
-ac = a-c;
-bd = b-d;
-ab = a-b;
-cd = c-d;
-sum = a+b+c+d;
+aMc = a-c;
+bMd = b-d;
+aMb = a-b;
+cMd = c-d;
 
-xy = [(ac-bd)./sum*Kx (ac+bd)./sum*Ky];
-q = (ab+cd)./sum*Kx;
-s = sum*Ks;
+aPb = a+b;
+cPd = c+d;
+
+sum = aPb+cPd;
+
+if strcmpi(method, 'delta/sigma')
+    xy = [Kx*(aMc-bMd)./sum Ky*(aMc+bMd)./sum];
+    q = (aMb+cMd)./sum;
+elseif strcmpi(method, 'partial delta/sigma')
+    aPc = a+c;
+    bPd = b+d;
+    
+    xy = 0.5*[Kx*(aMc./aPc - bMd./bPd) Ky*(aMc./aPc + bMd./bPd)];
+    q = 0.5*(aMb./aPb + cMd./cPd);
+end
+
+s = Ks*sum;
