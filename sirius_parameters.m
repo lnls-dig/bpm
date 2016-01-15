ca95f6adf451a7005f6c9fed827e2e612a1f547a
Daniel Tavares <daniel.tavares@lnls.br>
2016-01-15 20:06:48 -0200

    sirius_parameters.m: reconsidering 500 mA as worst case nominal beam current (multibunch)

--- sirius_parameters.m
+++ sirius_parameters.m
@@ -6,7 +6,7 @@ storagering = struct(...
     'circumference', 518.246, ...       % Ring circumference [m]
     'frf', 499.798e6, ...               % RF accelerating frequency [Hz]
     'bunchLength', 8.8e-12, ...         % Natural bunch length [s]
-    'beamCurrent', 350e-3, ...          % Beam current (multi bunch) [A] 
+    'beamCurrent', 500e-3, ...          % Beam current (multi bunch) [A] 
     'beamCurrentSB', 1e-3, ...          % Beam current (single bunch) [A] 
     'h', 864, ...                       % Harmonic number 
     'beta', 1 ...                       % Beam speed (fraction of c)
