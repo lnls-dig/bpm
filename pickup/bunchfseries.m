76097c00b3f51e010cd57027670a5145176a7322
Daniel Tavares <daniel.tavares@lnls.br>
2016-02-08 10:50:28 -0200

    bunchfseries.m: fix function help and comment texts

--- pickup/bunchfseries.m
+++ pickup/bunchfseries.m
@@ -1,29 +1,18 @@
 function [Ibeam, f] = bunchfseries(accelerator, Iavg, nbunches, nharmonicsRF)
-%BUNCHFSERIES Calculate Fourier Series of current and voltage signals for
-%   button BPMs (take antenna with higher singal amplitude).
+%BUNCHFSERIES Calculate Fourier Series of beam current.
 %
-%   r = bunchfseries(accelerator, bpm, Iavg, nbunches, beampos, nharmonicsRF)
+%   r = bunchfseries(accelerator, bpm, Iavg, nbunches, nharmonicsRF)
 %
 %   Inputs:
 %       accelerator: accelerator characteristics
-%       bpm: BPM characteristics
 %       Iavg: total average beam current [A]
 %       nbunches: number of bunches on the beam
-%       beampos: horizontal (beampos(1)) and vertical (beampos(2)) beam
-%                position [m] - optional (default = [0 0])
 %       nharmonicsRF: number of RF harmonics to take into account on the
 %                     analysis - optional (default = 100)
-%       nharmonicsRev: number of revolution harmonics to take into account
-%                      on the analysis - optional (default = 100)
 %
 %   Outputs:
-%       r.freq: frequency vector [Hz]
-%       r.Ibeam: Fourier series coefficients of beam current [A]
-%       r.Iim: Fourier series coefficients of beam image current [A] on BPM
-%              button where signal amplitude is the highest
-%       r.Vim: Fourier series coefficients of voltage on button [V]
-%       r.Vcable: Fourier series coefficients of voltage at the end of the
-%                 coaxial cable (RF front-end input) [V]
+%       Ibeam: Fourier series coefficients of beam current [A]
+%       f:     frequency vector [Hz]
 %
 %   See also FOURIERSERIES2TIME
 
@@ -47,7 +36,7 @@ frev = frf/h;
 % m is the index of the beam revolution harmonics
 m = 0:nharmonicsRF*h;
 
-% Frequency and angular frequency vectors
+% Frequency vectors
 f = frev*m;
 
 % Calculate the average bunch current
