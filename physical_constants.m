bf798e626d1f66f9fc83985e551d19165ae57c81
Daniel Tavares <daniel.tavares@lnls.br>
2016-02-09 18:14:41 -0200

    physical_constants.m: add Boltzmann constant

--- physical_constants.m
+++ physical_constants.m
@@ -5,7 +5,9 @@
 %       e: electron charge [C]
 %       e0: vacuum permittivity [F/m]
 %       c: speed of light [m/s]
+%       K: Boltzmann constant m^2*kg/s^2/K
 
 e = 1.60217733e-19;
 e0 = 8.854187817e-12;
 c = 2.99792458e8;
+K = 1.38064852e-23;
