04857e744562ffc3de552f47d0fa0f6489c48593
Daniel Tavares <daniel.tavares@lnls.br>
2018-10-11 17:20:48 -0300

    calcpos.m: add new position calculation methods

--- calcpos.m
+++ calcpos.m
@@ -38,6 +38,18 @@ elseif strcmpi(method, 'partial delta/sigma')
     x = 0.5*Kx*(aMc./aPc - bMd./bPd);
     y = 0.5*Ky*(aMc./aPc + bMd./bPd);
     q = 0.5*(aMb./aPb + cMd./cPd);
+elseif strcmpi(method, 'cross partial delta/sigma')
+    aPc = a+c;
+    bPd = b+d;
+    x = -0.5*Kx*(bMd./bPd);
+    y = 0.5*Ky*(aMc./aPc);
+    q = (aMb./aPb + cMd./cPd);
+elseif strcmpi(method, 'pi/pi')
+    aDc = a./c;
+    bDd = b./d;
+    x = 0.125*Kx*(aDc-1./aDc-(bDd-1./bDd));
+    y = 0.125*Ky*(aDc-1./aDc+bDd-1./bDd);
+    q = a./b + c./d;
 end
 
 xy = zeros(size(x,1), 2*size(x,2));
