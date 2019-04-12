840248d5d320ba4f794c1bd0bf021ff743bc0807
Daniel Tavares <daniel.tavares@lnls.br>
2019-04-11 23:55:21 -0300

    fit2dsvd.m: add option for weighted least squares fitting

--- pickup/fit2dsvd.m
+++ pickup/fit2dsvd.m
@@ -1,12 +1,21 @@
-function coeff = fit2dsvd(x, y, z, coeff_desc, cond_tol)
+function coeff = fit2dsvd(x, y, z, coeff_desc, cond_tol, W)
 
 if nargin < 5 || isempty(cond_tol)
     cond_tol = Inf;
+end
+if nargin < 6 || isempty(W)
+    W = [];
 end
 
 M = fit2dsvdmatrix(x,y,coeff_desc);
 
 z = z(:);
+
+if ~isempty(W)
+    W = diag(W(:));
+    M = W*M;
+    z = W*z;
+end
 
 sv = svd(M);
 cond = sv(1)./sv(2:end);
